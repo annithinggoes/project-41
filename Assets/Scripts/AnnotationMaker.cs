87b2bbb2cffc119b448bd750c2c6a8ccc8526f23
dongmeilim <dongmeilim@gmail.com>
2021-08-18 23:00:42 +1200

    Add approve/reject functions and assets

---
+++ Assets/Scripts/AnnotationMaker.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine;
+
+public class AnnotationMaker : MonoBehaviour
+{
+    public GameObject approveMarker;
+    public GameObject rejectMarker;
+    public void approveHighlightedObject()
+    {
+        if (CoreServices.InputSystem.GazeProvider.GazeTarget)
+        {
+            GameObject target = CoreServices.InputSystem.GazeProvider.GazeTarget;
+            Transform targetParent = target.transform.parent;
+            if (targetParent.name.Contains("Questionable") && targetParent.transform.Find("ApprovalMarker(Clone)") == null)
+            {
+                if (targetParent.transform.Find("RejectionMarker(Clone)") != null)
+                {
+                    Destroy(targetParent.transform.Find("RejectionMarker(Clone)").gameObject);
+                }
+                GameObject clone;
+                Vector3 offset = new Vector3(0, 0.06f, 0);
+                clone = Instantiate(approveMarker, target.transform.position + offset, target.transform.rotation);
+                clone.transform.parent = targetParent;
+
+            }
+
+        }
+    }
+    public void rejectHighlightedObject()
+    {
+        if (CoreServices.InputSystem.GazeProvider.GazeTarget)
+        {
+            GameObject target = CoreServices.InputSystem.GazeProvider.GazeTarget;
+            Transform targetParent = target.transform.parent;
+            if (targetParent.name.Contains("Questionable") && targetParent.transform.Find("RejectionMarker(Clone)") == null)
+            {
+                if (targetParent.transform.Find("ApprovalMarker(Clone)") != null)
+                {
+                    Destroy(targetParent.transform.Find("ApprovalMarker(Clone)").gameObject);
+                }
+                GameObject clone;
+                Vector3 offset = new Vector3(0, 0.06f, 0);
+                clone = Instantiate(rejectMarker, target.transform.position + offset, target.transform.rotation);
+                clone.transform.parent = targetParent;
+
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    private Color main;
+
+    void Start() {
+        main = gameObject.GetComponent<Renderer>().material.color;
+    }
+    public void changeFocusColor()
+    {
+        var renderer = gameObject.GetComponent<Renderer>();
+
+       //Call SetColor using the shader property name "_Color" and setting the color to red
+       renderer.material.SetColor("_Color", Color.cyan);
+    }
+    public void changeToMainColor()
+    {
+        var renderer = gameObject.GetComponent<Renderer>();
+
+       //Call SetColor using the shader property name "_Color" and setting the color to red
+       renderer.material.SetColor("_Color", main);
+    }
+}
