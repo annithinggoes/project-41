88b1ee01c0d770c849923c7617b14d2bd2fa0ada
dongmeilim <dongmeilim@gmail.com>
2021-08-19 18:51:44 +1200

    add gui to marker and refactor annotationmaker

--- Assets/Scripts/AnnotationMaker.cs
+++ Assets/Scripts/AnnotationMaker.cs
@@ -8,44 +8,42 @@ public class AnnotationMaker : MonoBehaviour
     public GameObject rejectMarker;
     public void approveHighlightedObject()
     {
-        if (CoreServices.InputSystem.GazeProvider.GazeTarget)
-        {
-            GameObject target = CoreServices.InputSystem.GazeProvider.GazeTarget;
-            Transform targetParent = target.transform.parent;
-            if (targetParent.name.Contains("Questionable") && targetParent.transform.Find("ApprovalMarker(Clone)") == null)
-            {
-                if (targetParent.transform.Find("RejectionMarker(Clone)") != null)
-                {
-                    Destroy(targetParent.transform.Find("RejectionMarker(Clone)").gameObject);
-                }
-                GameObject clone;
-                Vector3 offset = new Vector3(0, 0.06f, 0);
-                clone = Instantiate(approveMarker, target.transform.position + offset, target.transform.rotation);
-                clone.transform.parent = targetParent;
-
-            }
-
-        }
+        annotateAndShowGUI(approveMarker, rejectMarker);
     }
     public void rejectHighlightedObject()
     {
+        annotateAndShowGUI(rejectMarker, approveMarker);
+    }
+
+    private void annotateAndShowGUI(GameObject addedMarker, GameObject removedMarker)
+    {
+        string addedMarkerName = addedMarker.name + "(Clone)";
+        string removedMarkerName = removedMarker.name + "(Clone)";
         if (CoreServices.InputSystem.GazeProvider.GazeTarget)
         {
             GameObject target = CoreServices.InputSystem.GazeProvider.GazeTarget;
-            Transform targetParent = target.transform.parent;
-            if (targetParent.name.Contains("Questionable") && targetParent.transform.Find("RejectionMarker(Clone)") == null)
+            Transform questionableObject;
+            if (!target.name.Contains("Questionable"))
             {
-                if (targetParent.transform.Find("ApprovalMarker(Clone)") != null)
+                questionableObject = target.transform.parent;
+            }
+            else
+            {
+                questionableObject = target.transform;
+            }
+            Debug.Log(target);
+            if (questionableObject.name.Contains("Questionable") && questionableObject.transform.Find(addedMarkerName) == null)
+            {
+                if (questionableObject.transform.Find(removedMarkerName) != null)
                 {
-                    Destroy(targetParent.transform.Find("ApprovalMarker(Clone)").gameObject);
+                    Destroy(questionableObject.transform.Find(removedMarkerName).gameObject);
                 }
                 GameObject clone;
-                Vector3 offset = new Vector3(0, 0.06f, 0);
-                clone = Instantiate(rejectMarker, target.transform.position + offset, target.transform.rotation);
-                clone.transform.parent = targetParent;
-
+                Vector3 offset = new Vector3(0, 0.08f, 0);
+                clone = Instantiate(addedMarker, target.transform.position + offset, target.transform.rotation);
+                clone.transform.parent = questionableObject;
+                questionableObject.transform.Find("CommentGUI").gameObject.SetActive(true);
             }
-
         }
     }
 }
