c63b5da736419f1fc78279bc20049e4b7f52e341
Annie Chau <annithinggoes@gmail.com>
2021-08-20 16:13:51 +1200

    angle and thumbs recognition logic

--- Assets/Scripts/GestureAngleRecogniser.cs
+++ Assets/Scripts/GestureAngleRecogniser.cs
@@ -1,47 +1,139 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
 using Microsoft;
 using Microsoft.MixedReality.Toolkit.Utilities;
 using Microsoft.MixedReality.Toolkit.Input;
 
 public class GestureAngleRecogniser : MonoBehaviour
 {
-    public GameObject angleMarker;
-
     /// <summary>
     /// Threshold for defining a curled finger
     /// </summary>
-    public float curlThreshold;
+    public float angleCurlThreshold;
 
     /// <summary>
     /// Threshold for defining a straight finger
     /// </summary>
-    public float straightThreshold;
+    public float angleStraightThreshold;
+    public float thumbsUpCurlThreshold;
+    public float thumbsUpStraightThreshold;
 
-    GameObject cube;
+    public TextMeshPro textMeshPro;
+    public TextMeshPro textMeshProHit;
+    public TextMeshPro textMeshProWrist;
+    public TextMeshPro textMeshProDebugger;
+
+    Handedness rightHand = Handedness.Right;
+
     // Start is called before the first frame update
     void Start()
     {
-        cube = Instantiate(angleMarker, this.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        cube.GetComponent<Renderer>().enabled = false;
-
-        Handedness hand = Handedness.Right;
-
-        if (HandPoseUtils.ThumbFingerCurl(hand) < straightThreshold &&
-        HandPoseUtils.IndexFingerCurl(hand) < straightThreshold &&
-        HandPoseUtils.MiddleFingerCurl(hand) > curlThreshold &&
-        HandPoseUtils.RingFingerCurl(hand) > curlThreshold &&
-        HandPoseUtils.PinkyFingerCurl(hand) > curlThreshold)
+        // Check if hand in view
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, rightHand, out MixedRealityPose pose))
         {
-            cube.GetComponent<Renderer>().enabled = true;
-            cube.transform.position = new Vector3(0,0,0.5f);
+            if (checkAngle())
+            {
+                textMeshProHit.SetText("ANGLE");
+            }
+            else if (checkThumbs())
+            {
+
+            }
+            else
+            {
+                textMeshProHit.SetText("NONE");
+            }
+
+            // Debug.Log("Thumb" + HandPoseUtils.ThumbFingerCurl(hand));
+            // Debug.Log("Index" + HandPoseUtils.IndexFingerCurl(hand));
+            textMeshPro.SetText(
+                "Thumb " + HandPoseUtils.ThumbFingerCurl(rightHand)
+                + "\nIndex " + HandPoseUtils.IndexFingerCurl(rightHand)
+                + "\nMiddle " + HandPoseUtils.MiddleFingerCurl(rightHand)
+                + "\nRing " + HandPoseUtils.RingFingerCurl(rightHand)
+                + "\nPinky " + HandPoseUtils.PinkyFingerCurl(rightHand)
+                );
+
+            if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Wrist, rightHand, out MixedRealityPose wristPose))
+            {
+                textMeshProWrist.SetText("Wrist\n Position " + wristPose.Position
+                + "\nRotation " + wristPose.Rotation);
+            }
+
         }
+    }
+
+    bool checkAngle()
+    {
+        Debug.Log("checkAngle");
+        if (HandPoseUtils.ThumbFingerCurl(rightHand) <= angleStraightThreshold &&
+        HandPoseUtils.IndexFingerCurl(rightHand) <= angleStraightThreshold &&
+        HandPoseUtils.MiddleFingerCurl(rightHand) > angleCurlThreshold &&
+        HandPoseUtils.RingFingerCurl(rightHand) > angleCurlThreshold &&
+        HandPoseUtils.PinkyFingerCurl(rightHand) > angleCurlThreshold)
+        {
+            textMeshProHit.SetText("ANGLE");
+            Debug.Log("L HIT");
+            return true;
+        }
+        return false;
+    }
+    bool checkThumbs()
+    {
+        Debug.Log("checkThumbsUp");
+
+        if (HandPoseUtils.ThumbFingerCurl(rightHand) <= thumbsUpStraightThreshold &&
+        HandPoseUtils.IndexFingerCurl(rightHand) > thumbsUpCurlThreshold &&
+        HandPoseUtils.MiddleFingerCurl(rightHand) > thumbsUpCurlThreshold &&
+        HandPoseUtils.RingFingerCurl(rightHand) > thumbsUpCurlThreshold &&
+        HandPoseUtils.PinkyFingerCurl(rightHand) > thumbsUpCurlThreshold)
+        {
+            if (isThumbUp())
+            {
+                textMeshProHit.SetText("Thumbs UP");
+                Debug.Log("THUMB HIT");
+                return true;
+            }
+            else if (isThumbDown())
+            {
+                textMeshProHit.SetText("Thumbs DOWN");
+                return true;
+            }
+        }
+
+        return false;
+    }
+    // 0 - 60, thumbdpwn is 120- 180
+    private bool isThumbUp()
+    {
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.ThumbTip, rightHand, out MixedRealityPose thumbTipPose) && HandJointUtils.TryGetJointPose(TrackedHandJoint.ThumbProximalJoint, rightHand, out MixedRealityPose thumbProximalPose))
+        {
+            Vector3 thumbDirection = thumbTipPose.Position - thumbProximalPose.Position;
+            float thumbCameraAngle = Vector3.Angle(thumbDirection, CameraCache.Main.transform.up);
+
+            return thumbCameraAngle < 60;
+
+        }
+        return false;
+    }
+    private bool isThumbDown()
+    {
+        if (HandJointUtils.TryGetJointPose(TrackedHandJoint.ThumbTip, rightHand, out MixedRealityPose thumbTipPose) && HandJointUtils.TryGetJointPose(TrackedHandJoint.ThumbProximalJoint, rightHand, out MixedRealityPose thumbProximalPose))
+        {
+            Vector3 thumbDirection = thumbTipPose.Position - thumbProximalPose.Position;
+            float thumbCameraAngle = Vector3.Angle(thumbDirection, CameraCache.Main.transform.up);
+
+            return thumbCameraAngle > 120;
+
+        }
+        return false;
     }
 }
