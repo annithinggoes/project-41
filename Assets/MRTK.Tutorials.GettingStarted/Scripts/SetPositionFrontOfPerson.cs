45c05bcb78398f8a62b18cbd1bc92615b2066d1c
dongmeilim <dongmeilim@gmail.com>
2021-08-24 20:04:21 +1200

    add plans

---
+++ Assets/MRTK.Tutorials.GettingStarted/Scripts/SetPositionFrontOfPerson.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Utilities;
+public class SetPositionFrontOfPerson : MonoBehaviour
+{
+    public float distance = 1f;
+    public void SetPosition()
+    {
+        transform.rotation = Camera.main.transform.rotation;
+        transform.position = CameraCache.Main.transform.position + CameraCache.Main.transform.forward*distance;
+    }
+}
