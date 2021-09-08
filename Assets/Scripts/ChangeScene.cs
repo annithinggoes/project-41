fd26166380e034a87b6ab8da44d110439f3f28d5
dongmeilim <dongmeilim@gmail.com>
2021-09-08 19:25:59 +1200

    remove camera code from scenes

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -7,14 +7,6 @@ public class ChangeScene : MonoBehaviour
 {
     public void changeScene(string sceneName)
     {
-        PlayerPrefs.SetFloat("RotationX", Camera.main.transform.eulerAngles.x);
-        PlayerPrefs.SetFloat("RotationY", Camera.main.transform.eulerAngles.y);
-        PlayerPrefs.SetFloat("RotationZ", Camera.main.transform.eulerAngles.z);
-        PlayerPrefs.SetFloat("PositionX", Camera.main.transform.position.x);
-        PlayerPrefs.SetFloat("PositionY", Camera.main.transform.position.y);
-        PlayerPrefs.SetFloat("PositionZ", Camera.main.transform.position.z);
-        Debug.Log(Camera.main.transform.rotation);
-        Debug.Log(Camera.main.transform.position);
         SceneManager.LoadScene(sceneName);
     }
 }
